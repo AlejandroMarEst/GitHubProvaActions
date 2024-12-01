ddb3f6d475a426f6ea31467e149cbadd4fb3787f
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-12-01 14:36:59 +0100

    Creat codi saludar usuari

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ProvaGitHubActions
+{
+    public class ProvaGitHubActions
+    {
+        public static void Main()
+        {
+            const string MSG1 = "Hola! Inserta el teu nom:";
+            const string MSG2 = "Hola {0}!";
+            const string ErrorMsg = "El valor introduit no es valid: ";
+            string nameUser;
+            Console.WriteLine(MSG1);
+            try
+            {
+                nameUser = Console.ReadLine();
+                Console.WriteLine(MSG2, nameUser);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(ErrorMsg);
+            }
+        }
+    }
+}
